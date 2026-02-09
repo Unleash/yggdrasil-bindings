cc0501b0595bec53559d9bc2d462f29d5c855617
David Leek <david@getunleash.io>
2026-02-09 14:46:47 +0100

    chore: add test for DefineCounter

---
+++ dotnet-engine/Yggdrasil.Engine.Tests/YggdrasilImpactMetricsTest.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text.Json;
+using NUnit.Framework;
+using Yggdrasil;
+using Yggdrasil.Test;
+
+public class YggdrasilImpactMetricsTest
+{
+    private JsonSerializerOptions options = new JsonSerializerOptions
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
+    
+    [Test]
+    public void DefineCounter_Throws_Only_When_Invalid()
+    {
+        var yggdrasilEngine = new YggdrasilEngine();
+        Assert.DoesNotThrow(() => yggdrasilEngine.DefineCounter("server_time", "Measures time spent on server doing things"));
+        Assert.Throws<YggdrasilEngineException>(() => yggdrasilEngine.DefineCounter("server_time", null!));
+        Assert.Throws<YggdrasilEngineException>(() => yggdrasilEngine.DefineCounter(null!, "Measures time spent on server doing things"));
+        Assert.Throws<YggdrasilEngineException>(() => yggdrasilEngine.DefineCounter(null!, null!));
+    }
+}
