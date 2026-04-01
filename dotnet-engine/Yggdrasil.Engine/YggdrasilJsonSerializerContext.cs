08812de40d1e6c681c5131134539d7e0df8833f6
Jacob Millward-Yuan <jacob@seatfrog.com>
2026-04-01 16:03:22 +0100

    fix(dotnet): remove duplicate JsonSerializable entry for JsonElement

--- dotnet-engine/Yggdrasil.Engine/YggdrasilJsonSerializerContext.cs
+++ dotnet-engine/Yggdrasil.Engine/YggdrasilJsonSerializerContext.cs
@@ -6,10 +6,9 @@ using System.Text.Json.Serialization;
 namespace Yggdrasil;
 
 [JsonSerializable(typeof(EngineResponse<object>))]
-[JsonSerializable(typeof(System.Text.Json.JsonElement))]
+[JsonSerializable(typeof(JsonElement))]
 [JsonSerializable(typeof(FeatureCollection))]
 [JsonSerializable(typeof(Dictionary<string, bool>))]
-[JsonSerializable(typeof(JsonElement))]
 [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
 internal partial class YggdrasilJsonSerializerContext : JsonSerializerContext
 {
