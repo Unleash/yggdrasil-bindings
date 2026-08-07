57c4ffd5c4f154509af104aabd2979dab170ae85
Simon Hornby <liquidwicked64@gmail.com>
2026-08-07 15:26:28 +0200

    chore(java): move binaries to versioned artifacts

--- java-engine/src/main/java/io/getunleash/engine/NativeLoader.java
+++ java-engine/src/main/java/io/getunleash/engine/NativeLoader.java
@@ -4,17 +4,17 @@ import java.io.*;
 
 final class NativeLoader {
 
-  static void loadFromResources(String libName) {
-    try (var in = NativeLoader.class.getResourceAsStream("/native/" + libName)) {
-      if (in == null) throw new IllegalStateException("Missing /native/" + libName);
-      var tmp = java.nio.file.Files.createTempFile("ygg_", "_" + libName);
+  static void loadFromResources(LibNames.NativeLibrary library) {
+    try (var in = NativeLoader.class.getResourceAsStream(library.resourcePath())) {
+      if (in == null) throw new IllegalStateException("Missing " + library.resourcePath());
+      var tmp = java.nio.file.Files.createTempFile("ygg_", "_" + library.fileName());
       tmp.toFile().deleteOnExit();
       try (var out = java.nio.file.Files.newOutputStream(tmp)) {
         in.transferTo(out);
       }
       System.load(tmp.toAbsolutePath().toString());
     } catch (Exception e) {
-      throw new RuntimeException("Failed to load native lib " + libName, e);
+      throw new RuntimeException("Failed to load native lib " + library.fileName(), e);
     }
   }
 }
