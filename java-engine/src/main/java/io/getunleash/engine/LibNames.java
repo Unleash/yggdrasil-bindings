bbe81bf8d1b3a41af9f49bf0f33db0dd6548af9f
Simon Hornby <liquidwicked64@gmail.com>
2026-08-13 15:42:56 +0200

    chore: attempt to load from system paths

--- java-engine/src/main/java/io/getunleash/engine/LibNames.java
+++ java-engine/src/main/java/io/getunleash/engine/LibNames.java
@@ -9,27 +9,30 @@ final class LibNames {
   static NativeLibrary pickForCurrentOsArch(String version) {
     String os = System.getProperty("os.name").toLowerCase(Locale.ROOT);
     String arch = System.getProperty("os.arch").toLowerCase(Locale.ROOT);
+    String libraryName = "yggdrasilffi-" + version;
     if (os.contains("mac")) {
       return arch.contains("aarch64") || arch.contains("arm64")
-          ? new NativeLibrary("macos-arm64", "libyggdrasilffi-" + version + ".dylib")
-          : new NativeLibrary("macos-x86_64", "libyggdrasilffi-" + version + ".dylib");
+          ? new NativeLibrary("macos-arm64", "libyggdrasilffi-" + version + ".dylib", libraryName)
+          : new NativeLibrary("macos-x86_64", "libyggdrasilffi-" + version + ".dylib", libraryName);
     } else if (os.contains("win")) {
       if (arch.contains("arm64")) {
-        return new NativeLibrary("windows-arm64", "yggdrasilffi-" + version + ".dll");
+        return new NativeLibrary("windows-arm64", "yggdrasilffi-" + version + ".dll", libraryName);
       }
       if (arch.contains("64")) {
-        return new NativeLibrary("windows-x86_64", "yggdrasilffi-" + version + ".dll");
+        return new NativeLibrary("windows-x86_64", "yggdrasilffi-" + version + ".dll", libraryName);
       }
-      return new NativeLibrary("windows-i686", "yggdrasilffi-" + version + ".dll");
+      return new NativeLibrary("windows-i686", "yggdrasilffi-" + version + ".dll", libraryName);
     } else { // linux
       if (isMusl()) {
         return arch.contains("aarch64") || arch.contains("arm64")
-            ? new NativeLibrary("linux-arm64-musl", "libyggdrasilffi-" + version + ".so")
-            : new NativeLibrary("linux-x86_64-musl", "libyggdrasilffi-" + version + ".so");
+            ? new NativeLibrary(
+                "linux-arm64-musl", "libyggdrasilffi-" + version + ".so", libraryName)
+            : new NativeLibrary(
+                "linux-x86_64-musl", "libyggdrasilffi-" + version + ".so", libraryName);
       }
       return arch.contains("aarch64") || arch.contains("arm64")
-          ? new NativeLibrary("linux-arm64", "libyggdrasilffi-" + version + ".so")
-          : new NativeLibrary("linux-x86_64", "libyggdrasilffi-" + version + ".so");
+          ? new NativeLibrary("linux-arm64", "libyggdrasilffi-" + version + ".so", libraryName)
+          : new NativeLibrary("linux-x86_64", "libyggdrasilffi-" + version + ".so", libraryName);
     }
   }
 
@@ -57,10 +60,12 @@ final class LibNames {
   static final class NativeLibrary {
     private final String platformDirectory;
     private final String fileName;
+    private final String libraryName;
 
-    NativeLibrary(String platformDirectory, String fileName) {
+    NativeLibrary(String platformDirectory, String fileName, String libraryName) {
       this.platformDirectory = platformDirectory;
       this.fileName = fileName;
+      this.libraryName = libraryName;
     }
 
     String resourcePath() {
@@ -69,6 +74,10 @@ final class LibNames {
 
     String fileName() {
       return fileName;
+    }
+
+    String libraryName() {
+      return libraryName;
     }
   }
 }
--- java-engine/src/main/java/io/getunleash/engine/NativeLoader.java
+++ java-engine/src/main/java/io/getunleash/engine/NativeLoader.java
@@ -5,6 +5,14 @@ import java.io.*;
 final class NativeLoader {
 
   static void loadFromResources(LibNames.NativeLibrary library) {
+    try {
+      System.loadLibrary(library.libraryName());
+      return;
+    } catch (UnsatisfiedLinkError e) {
+      // was a long shot anyway but it's polite to try to load from standard system
+      // paths before we fallback to more invasive options
+    }
+
     try (var in = NativeLoader.class.getResourceAsStream(library.resourcePath())) {
       if (in == null) throw new IllegalStateException("Missing " + library.resourcePath());
       var tmp = java.nio.file.Files.createTempFile("ygg_", "_" + library.fileName());
